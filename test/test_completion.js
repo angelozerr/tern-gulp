30fced57a92487e759d900ba0fd448e3f463f554
angelozerr <angelo.zerr@gmail.com>
2015-04-06 23:00:36 +0200

    Add gulp completion test

--- test/test_completion.js
+++ test/test_completion.js
@@ -1,24 +1,13 @@
 var util = require("./util");
 
-exports['test Grunt (Not in Function) completion'] = function() {
-  util.assertCompletion("gulp.reg", {
-      "start":{"line":0,"ch":6},
-      "end":{"line":0,"ch":9},
+exports['test Gulp completion'] = function() {
+  util.assertCompletion("var gulp = require('gulp');gulp.ta", {
+      "start":{"line":0,"ch":32},
+      "end":{"line":0,"ch":34},
       "isProperty":true,
       "isObjectKey":false,
-      "completions":[]
+      "completions":[{"name":"task","type":"fn(name: string, deps?: [string], fn: fn())","origin":"gulp"}]
   });
-}
-
-exports['test Grunt completion'] = function() {
-	util.assertCompletion("module.exports = function(grunt) {grunt.reg", {
-	    "start":{"line":0,"ch":40},
-	    "end":{"line":0,"ch":43},
-	    "isProperty":true,
-	    "isObjectKey":false,
-	    "completions":[{"name":"registerMultiTask","type":"fn(taskName: string, description: string, taskFunction: fn())","origin":"grunt"},
-	                   {"name":"registerTask","type":"fn(taskName: string, taskList: [string])","origin":"grunt"}]
-	});
 }
 
 if (module == require.main) require("test").run(exports);
